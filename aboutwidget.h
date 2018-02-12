613efca656d5fd0e7f6e00da4dc11c2846f66ada
gcmwhite <itoday_06@qq.com>
2018-02-12 14:24:01 +0800

    about widget

---
+++ aboutwidget.h
@@ -0,0 +1,17 @@
+#ifndef ABOUTWIDGET_H
+#define ABOUTWIDGET_H
+
+#include <QWidget>
+
+class AboutWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit AboutWidget(QWidget *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // ABOUTWIDGET_H
