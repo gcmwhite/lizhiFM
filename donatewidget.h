9840aae622a7ffe9f04ba3a60ef222286c981e8f
gcmwhite <itoday_06@qq.com>
2018-02-12 14:24:25 +0800

    donate widget

---
+++ donatewidget.h
@@ -0,0 +1,17 @@
+#ifndef DONATEWIDGET_H
+#define DONATEWIDGET_H
+
+#include <QWidget>
+
+class DonateWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit DonateWidget(QWidget *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // DONATEWIDGET_H
